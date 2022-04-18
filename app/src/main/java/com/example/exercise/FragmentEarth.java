f4f0475a5b8630f918d911ba48e9846f0b995cbf
Taeeun Kim <taeeun13@khu.ac.kr>
2022-04-18 17:01:59 +0900

    Show earth temperatures in Celsius

--- app/src/main/java/com/example/exercise/FragmentEarth.java
+++ app/src/main/java/com/example/exercise/FragmentEarth.java
@@ -23,8 +23,8 @@ public class FragmentEarth extends Fragment {
     TextView earthDateTextView;
     TextView earthMaxTempView;
     TextView earthMinTempView;
-    String worldWeatherMapApiKey = "f7ba5036adfc078bc9d35926eb3b86ca";
-    String earthGeocodingApi = "http://api.openweathermap.org/geo/1.0/direct?q=Seoul&limit=5&appid=f7ba5036adfc078bc9d35926eb3b86ca";
+    //String worldWeatherMapApiKey = "f7ba5036adfc078bc9d35926eb3b86ca";
+    //String earthGeocodingApi = "http://api.openweathermap.org/geo/1.0/direct?q=Seoul&limit=5&appid=f7ba5036adfc078bc9d35926eb3b86ca";
     //Latitude = 37.532600, Longitude: 127.024612
     String earthApiURL = "https://api.openweathermap.org/data/2.5/weather?lat=37.5666791&lon=126.9782914&appid=f7ba5036adfc078bc9d35926eb3b86ca";
 
@@ -75,26 +75,19 @@ public class FragmentEarth extends Fragment {
             }
             br.close();
 
-            //Get Data
-//            String data = response.toString();
-//            Log.d("data", data);
-//            JSONObject jsonObj = new JSONObject(data);
-//            JSONArray jsonArr = (JSONArray) jsonObj.get("main");
-//            Log.d("data",jsonArr.getString(0));
-//            JSONObject object = jsonArr.getJSONObject(0);
-//            String date = object.getString("terrestrial_date");
-//            String marsMaxTempStr = object.getString("max_temp");
-//            String marsMinTempStr = object.getString("min_temp");
-//            Log.d("data", date);
             String earthData = response.toString();
             Log.d("data", earthData);
             JSONObject earthJsonObj = new JSONObject(earthData);
             JSONObject earthJsonMainObj = (JSONObject) earthJsonObj.get("main");
-            String earthMaxTempStr = earthJsonMainObj.getString("temp_max");
-            String earthMinTempStr = earthJsonMainObj.getString("temp_min");
+            double earthMaxTemp = earthJsonMainObj.getDouble("temp_max") - 273.15;
+            double earthMinTemp = earthJsonMainObj.getDouble("temp_min")  - 273.15;
 
-            earthMaxTempView.setText(earthMaxTempStr);
-            earthMinTempView.setText(earthMinTempStr);
+            earthMaxTemp = Math.round(earthMaxTemp * 10) / 10.0;
+            earthMinTemp = Math.round(earthMinTemp * 10) / 10.0;
+            String earthMaxTempStr = Double.toString(earthMaxTemp);
+            String earthMinTempStr = Double.toString(earthMinTemp);
+            earthMaxTempView.setText(earthMaxTempStr + "°");
+            earthMinTempView.setText(earthMinTempStr + "°");
 
         } catch  (Exception e) {
             e.printStackTrace();
